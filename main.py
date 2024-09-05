d11ebd97339da496f5fcf02e4f1207dd2ac25eaa
WankaIbrahim <224wankaibrahim@gmail.com>
2024-09-05 23:52:39 +0100

    Added aws secrets manager to get the open ai api key. The api key is gotten correctly but the llm doesnt work unless the api key is stored in the .env file. Need to fix this.

--- main.py
+++ main.py
@@ -1,15 +1,46 @@
 import os
 import pandas as pd
+import boto3
 from dotenv import load_dotenv
+from botocore.exceptions import ClientError
 from prompts import context
 from llama_index.core.agent import ReActAgent
-from llama_index.llms.openai import OpenAI
+import openai
+
+load_dotenv()
+
+def get_secret():
+
+
+    secret_name = "OPENAI_API_KEY"
+    region_name = "eu-north-1"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        # For a list of exceptions thrown, see
+        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        raise e
+
+    secret = get_secret_value_response['SecretString'].strip()      
+    return secret
+OPENAI_API_KEY = get_secret()
+print(OPENAI_API_KEY)
+
 from tools import tools, update_chat_history
 
-                                           
-load_dotenv()
-            
-llm = OpenAI(model="gpt-4o")
+
+llm = openai(model="gpt-4o",
+             api_key=OPENAI_API_KEY)
 
 agent = ReActAgent.from_tools(tools=tools,
                               llm=llm,
